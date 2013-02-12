c688b53d3e2cd042738440fdf14b33907a46b1e5
andyc <andyc@crytek.com>
2013-02-12 16:02:57 +0000

    added uber file to allow stdafx.h to be included with OpenGL SuperBible stuff, without changing the original files

---
+++ GLTools/src/openglsuperbible_uber.cpp
@@ -0,0 +1,9 @@
+#include "common/stdafx.h"
+
+#include "glew.c"
+#include "math3d.cpp"
+#include "GLTools.cpp"
+#include "GLBatch.cpp"
+#include "GLShaderManager.cpp"
+#include "GLTriangleBatch.cpp"
+
