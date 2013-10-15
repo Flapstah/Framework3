b0098b3100a03ca4e1ad5db7bc715164b49eff95
andyc <andyc@crytek.com>
2013-10-15 12:45:55 +0100

    redo platform type test

--- unit_tests/typetest.cpp
+++ unit_tests/typetest.cpp
@@ -34,34 +34,54 @@ namespace test
 
 	uint32 CTypeTest::SizeTest(CUnitTest* pParent)
 	{
+		char buffer[64];
 		CTypeTest* pThis = static_cast<CTypeTest*>(pParent);
+		uint32 status = eSS_PASS;
 
 		if (pThis->m_testStatus == eTS_RUNNING)
 		{
-#if defined(ENVIRONMENT32)
-			pThis->Log(eTV_VERBOSE, "(32 bit environment)");
-#endif // [defined(ENVIRONMENT32)]
-#if defined(ENVIRONMENT64)
-			pThis->Log(eTV_VERBOSE, "(64 bit environment)");
-#endif // [defined(ENVIRONMENT64)]
-#define LOG_SIZE(_type_) pThis->Log(eTV_VERBOSE, #_type_ "\t: %u", static_cast<uint32>(sizeof(_type_)))
-			LOG_SIZE(int8);
-			LOG_SIZE(int16);
-			LOG_SIZE(int32);
-			LOG_SIZE(int64);
-			LOG_SIZE(uint8);
-			LOG_SIZE(uint16);
-			LOG_SIZE(uint32);
-			LOG_SIZE(uint64);
-			LOG_SIZE(bool);
-			LOG_SIZE(float);
-			LOG_SIZE(double);
-			LOG_SIZE(size_t);
-			LOG_SIZE(void*);
-#undef LOG_SIZE
+			switch (pThis->GetStage())
+			{
+			case 1:
+				sprintf(buffer, __ENVIRONMENT__ " environment");
+				pThis->Test(buffer, true, NULL, eTT_SubStage);
+
+#define CREATE_SIZE_MESSAGE(_type_) sprintf(buffer, #_type_ "\t: %u", static_cast<uint32>(sizeof(_type_)))
+				CREATE_SIZE_MESSAGE(int8);
+				pThis->Test(buffer, true, NULL, eTT_SubStage);
+				CREATE_SIZE_MESSAGE(int16);
+				pThis->Test(buffer, true, NULL, eTT_SubStage);
+				CREATE_SIZE_MESSAGE(int32);
+				pThis->Test(buffer, true, NULL, eTT_SubStage);
+				CREATE_SIZE_MESSAGE(int64);
+				pThis->Test(buffer, true, NULL, eTT_SubStage);
+				CREATE_SIZE_MESSAGE(uint8);
+				pThis->Test(buffer, true, NULL, eTT_SubStage);
+				CREATE_SIZE_MESSAGE(uint16);
+				pThis->Test(buffer, true, NULL, eTT_SubStage);
+				CREATE_SIZE_MESSAGE(uint32);
+				pThis->Test(buffer, true, NULL, eTT_SubStage);
+				CREATE_SIZE_MESSAGE(uint64);
+				pThis->Test(buffer, true, NULL, eTT_SubStage);
+				CREATE_SIZE_MESSAGE(bool);
+				pThis->Test(buffer, true, NULL, eTT_SubStage);
+				CREATE_SIZE_MESSAGE(float);
+				pThis->Test(buffer, true, NULL, eTT_SubStage);
+				CREATE_SIZE_MESSAGE(double);
+				pThis->Test(buffer, true, NULL, eTT_SubStage);
+				CREATE_SIZE_MESSAGE(size_t);
+				pThis->Test(buffer, true, NULL, eTT_SubStage);
+				CREATE_SIZE_MESSAGE(void*);
+				pThis->Test(buffer, true, NULL);
+				break;
+
+			default:
+				status |= eSS_COMPLETE;
+				break;
+			}
 		}
 
-		return eSS_PASS | eSS_COMPLETE;
+		return status;
 	}
 
 	//============================================================================
